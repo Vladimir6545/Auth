5fecfe398bc1acbb449ec94edb1dea0515abfdf3
Vladimir6545 <kvv.login@gmail.com>
2020-05-09 23:03:25 +0300

    create project

---
+++ Auth/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Auth
+{
+    public class User
+    {
+        public string SubjectId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<Claim> Claims { get; set; }
+    }
+}
---
+++ Auth/Users.cs
@@ -0,0 +1,27 @@
+﻿using IdentityModel;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Auth
+{
+    public class Users
+    {
+        public List<User> Get()
+        {
+            return new List<User>
+            {
+                new User
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "Jon",
+                    Password = "password",
+                    Claims = new List<Claim> 
+                    {
+                        new Claim(JwtClaimTypes.Email, "kvv.login@gmail.com"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+        }
+    }
+}
